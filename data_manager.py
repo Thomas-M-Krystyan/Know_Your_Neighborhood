171aad5b3cfdc07259a58eb5c8422a0e85986b52
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-01-24 05:16:13 +0100

    Update the return and instantialization in the table_converter() function in 'data_manager.py'

--- data_manager.py
+++ data_manager.py
@@ -1,26 +1,31 @@
 import re
 
 # Modules of the current program.
-from administrative_statistics import Voivodeship
+from statistics import *
 
 
 def csv_reader(data_file_name):
     """
     Method reads CSV files and returns their content as a list of lists which contains a strings.
+
+    Returns:
+        list of lists (with strings)
     """
     file_content = []
-    with open(data_file_name, "r") as file:
+
+    with open(data_file_name, "r", encoding="utf-8") as file:
         next(file)
         for line in file:
-            clean_line = (re.split(',|\t|\n', line.strip()))  # Clearing every line of data list (removing ",", "\t", "\n") with RegEx.
+            clean_line = (re.split(',|;|:|\t|\n', line.strip()))  # Clearing every line of data list (removing separators and "\t", "\n") with RegEx.
             file_content.append(clean_line)
 
     return file_content
 
 
-def table_converter(file_content):
+def table_converter(data_file):
     """
-    Convert the content of a file (list of lists) to the objects ("list_of_current_location").
+    Convert the content of a file (list of lists) to the list
+    of objects in Voivodeship class from "statistics.py" file.
 
     Reference to the following elements from list:
 
@@ -30,15 +35,14 @@ def table_converter(file_content):
         [2] = ID of the community
         [3] = ID of the type of community
         [4] = Name of the region
-        [5] = Name of the region type
+        [5] = Name of the region's type
 
     Returns:
-        list of objects
+        None
     """
-    list_of_current_location = []
-
-    for element in file_content:
+    for element in data_file:
         current_location = Voivodeship(element[0], element[1], element[2], element[3], element[4], element[5])
-        list_of_current_location.append(current_location)
 
-    return list_of_current_location
+        instance = LocationList()
+
+        instance.add_location(current_location)
